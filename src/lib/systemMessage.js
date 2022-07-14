ffcf50286e192e4257a5bb775554a6d5a90c0910
Hyuntak Lee <ttttaakk620@gmail.com>
2022-07-14 23:34:40 +0900

    Debug Test_Firebase Api

--- src/lib/systemMessage.js
+++ src/lib/systemMessage.js
@@ -6,5 +6,5 @@ export async function getSystemMsg(){
     const result = await systemMsgCollection.doc('sample').get();
     return result.data();
     
-    //console.log('시스템 메시지=', result.data());
+    console.log('시스템 메시지=', result.data());
 }
